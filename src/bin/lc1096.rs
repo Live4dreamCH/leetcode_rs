3527e04e7b2bc73e5bad39395420055dde876d73
Tom <live4dreamch@gmail.com>
2023-04-05 17:06:09 +0800

    引入petgraph库来做语法树？

--- src/bin/lc1096.rs
+++ src/bin/lc1096.rs
@@ -3,7 +3,7 @@ impl Solution {
     pub fn brace_expansion_ii(expression: String) -> Vec<String> {
         // let mut ll1 = ll1::LL1::new();
         // let parse_tree = ll1.parse(&expression);
-        let mut tokenizer = cfg::Tokenizer::from(expression.as_bytes().iter());
+        let mut tokenizer = Tokenizer::from(expression.as_bytes().iter());
         for token in tokenizer {
             dbg!(token);
         }
@@ -11,92 +11,98 @@ impl Solution {
     }
 }
 
-pub mod cfg {
-    use crate::cfg::Token::*;
+#[derive(Debug)]
+pub enum Token {
+    OpenBrace,
+    Word(String),
+    Comma,
+    CloseBrace,
+}
+pub struct Tokenizer<'a> {
+    str_iter: core::slice::Iter<'a, u8>,
+}
+impl<'a> Tokenizer<'a> {
+    pub fn from(str_iter: core::slice::Iter<'a, u8>) -> Tokenizer {
+        Tokenizer { str_iter }
+    }
+}
+impl Iterator for Tokenizer<'_> {
+    type Item = Token;
 
-    #[derive(Debug)]
-    pub enum Token {
-        OpenBrace,
-        Word(String),
-        Comma,
-        CloseBrace,
-    }
-    pub struct Tokenizer<'a> {
-        str_iter: core::slice::Iter<'a, u8>,
-    }
-    impl<'a> Tokenizer<'a> {
-        pub fn from(str_iter: core::slice::Iter<'a, u8>) -> Tokenizer {
-            Tokenizer { str_iter }
-        }
-    }
-    impl Iterator for Tokenizer<'_> {
-        type Item = Token;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            let mut word = String::new();
-            match self.str_iter.next()? {
-                b'{' => Some(OpenBrace),
-                b',' => Some(Comma),
-                b'}' => Some(CloseBrace),
-                ch @ b'a'..=b'z' => {
-                    word.push(*ch as char);
-                    loop {
-                        let next_ch = self.str_iter.clone().next();
-                        if let Some(ch @ b'a'..=b'z') = next_ch {
-                            word.push(*ch as char);
-                            self.str_iter.next();
-                            continue;
-                        } else {
-                            return Some(Word(word));
-                        }
+    fn next(&mut self) -> Option<Self::Item> {
+        use Token::*;
+        let mut word = String::new();
+        match self.str_iter.next()? {
+            b'{' => Some(OpenBrace),
+            b',' => Some(Comma),
+            b'}' => Some(CloseBrace),
+            ch @ b'a'..=b'z' => {
+                word.push(*ch as char);
+                loop {
+                    let next_ch = self.str_iter.clone().next();
+                    if let Some(ch @ b'a'..=b'z') = next_ch {
+                        word.push(*ch as char);
+                        self.str_iter.next();
+                        continue;
+                    } else {
+                        return Some(Word(word));
                     }
                 }
-                error => panic!("meet char {}", error),
             }
+            error => panic!("meet char {}", error),
         }
     }
-
-    enum Symbol {
-        Expr,
-        Union,
-        Concat,
-        Id(String),
-    }
-    struct ParseTreeNode {
-        curr: Symbol,
-        children: Vec<Symbol>,
-    }
-    pub struct ParseTree {
-        root: ParseTreeNode,
-    }
-    impl ParseTree {
-        pub fn new() -> ParseTree {
-            ParseTree {
-                root: ParseTreeNode {
-                    curr: Symbol::Expr,
-                    children: Vec::new(),
-                },
-            }
-        }
-    }
-    // pub struct AST{}
 }
 
-pub mod parser {
-    use super::cfg::{ParseTree, Token};
-    pub struct RecursiveDecent {}
-    impl RecursiveDecent {
-        pub fn from() -> RecursiveDecent {
-            RecursiveDecent {}
-        }
-        pub fn parse() -> ParseTree {
-            let mut tree = ParseTree::new();
-            tree
-        }
-        // expr ->union | union,expr
-        // union->concat | concat union
-        // concat->word | {expr}
+enum Symbol {
+    Expr,
+    Union,
+    Concat,
+    Id(String),
+}
+use petgraph::graph::DiGraph;
+
+// pub struct ParseTreeNode {
+//     curr: Symbol,
+//     children: Vec<Symbol>,
+// }
+// pub struct ParseTree {
+//     root: ParseTreeNode,
+// }
+// impl ParseTree {
+//     pub fn new() -> ParseTree {
+//         ParseTree {
+//             root: ParseTreeNode {
+//                 curr: Symbol::Expr,
+//                 children: Vec::new(),
+//             },
+//         }
+//     }
+// }
+// pub struct AST{}
+
+pub struct RecursiveDecent {}
+impl RecursiveDecent {
+    pub fn from() -> RecursiveDecent {
+        RecursiveDecent {}
     }
+    pub fn parse(&self, tokenizer: &mut Tokenizer) -> DiGraph<Symbol, ()> {
+        let mut tree = DiGraph::new();
+        self.expr();
+        tree.;
+        tree
+    }
+    // expr ->union | union,expr
+    fn expr(&self) {
+        let mut node = ParseTreeNode {
+            curr: Symbol::Expr,
+            children: Vec::new(),
+        };
+    }
+    fn union(&self) {}
+    fn concat(&self) {}
+    // union->concat | concat union
+    // concat->word | {expr}
 }
 
 fn main() {
