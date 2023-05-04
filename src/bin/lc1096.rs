5ff7cf81fab53729991ad1652f701d2f17ea72f8
tom <tom@stu.xjtu.edu.cn>
2023-05-04 12:55:56 +0800

    回溯的准备工作

--- src/bin/lc1096.rs
+++ src/bin/lc1096.rs
@@ -1,9 +1,12 @@
+use std::collections::VecDeque;
+
 struct Solution;
+
 impl Solution {
     pub fn brace_expansion_ii(expression: String) -> Vec<String> {
         // let mut ll1 = ll1::LL1::new();
         // let parse_tree = ll1.parse(&expression);
-        let mut tokenizer = Tokenizer::from(expression.as_bytes().iter());
+        let mut tokenizer = Lexer::from(expression.as_bytes().iter());
         for token in tokenizer {
             dbg!(token);
         }
@@ -11,6 +14,7 @@ impl Solution {
     }
 }
 
+/// interface between lexer and parser
 #[derive(Debug)]
 pub enum Token {
     OpenBrace,
@@ -18,15 +22,27 @@ pub enum Token {
     Comma,
     CloseBrace,
 }
-pub struct Tokenizer<'a> {
+
+/// comsume string, and provide next token and lookahead function
+pub struct Lexer<'a> {
     str_iter: core::slice::Iter<'a, u8>,
 }
-impl<'a> Tokenizer<'a> {
-    pub fn from(str_iter: core::slice::Iter<'a, u8>) -> Tokenizer {
-        Tokenizer { str_iter }
+
+impl<'a> Lexer<'a> {
+    pub fn from(str_iter: core::slice::Iter<'a, u8>) -> Lexer {
+        Lexer { str_iter }
+    }
+
+    pub fn lookahead(&mut self, n: u32) -> Vec<Token> {
+        if 0 == n {
+            return Vec::new();
+        }
+        let new_self = Lexer::from(self.str_iter.clone());
+        new_self.take(n as usize).collect()
     }
 }
-impl Iterator for Tokenizer<'_> {
+
+impl Iterator for Lexer<'_> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -66,9 +82,11 @@ pub struct ParseTreeNode {
     curr: Symbol,
     children: Vec<Symbol>,
 }
+
 pub struct ParseTree {
     root: ParseTreeNode,
 }
+
 impl ParseTree {
     pub fn from(root: ParseTreeNode) -> ParseTree {
         ParseTree { root }
@@ -76,32 +94,42 @@ impl ParseTree {
 }
 // pub struct AST{}
 
-pub struct RecursiveDecent {}
+pub struct RecursiveDecent {
+    visited_path: VecDeque<(Symbol, bool)>,
+}
 impl RecursiveDecent {
-    pub fn from() -> RecursiveDecent {
-        RecursiveDecent {}
+    pub fn new() -> RecursiveDecent {
+        RecursiveDecent {
+            visited_path: VecDeque::new(),
+        }
     }
-    pub fn parse(&self, tokenizer: &mut Tokenizer) -> Result<ParseTree, String> {
-        let mut root = self.expr()?;
+
+    pub fn parse(&self, lexer: &mut Lexer) -> Result<ParseTree, String> {
+        let mut root = self.expr(lexer)?;
         Ok(ParseTree::from(root))
     }
+
     // expr ->union | union,expr
-    fn expr(&self) -> Result<ParseTreeNode, String> {
+    fn expr(&self, lexer: &mut Lexer) -> Result<ParseTreeNode, String> {
         let mut node = ParseTreeNode {
             curr: Symbol::Expr,
             children: Vec::new(),
         };
-        let mut err_vec = Vec::new();
-        match self.union() {
-            Ok(subtree) => ,
-            Err(_) => todo!(),
-        }
-        node
+
+        // let mut err_vec = Vec::new();
+        //  self.union() ?
+        Ok(node)
     }
+
     // union->concat | concat union
-    fn union(&self) -> Result<ParseTreeNode, String> {}
+    fn union(&self, lexer: &mut Lexer) -> Result<ParseTreeNode, String> {
+        Err(String::new())
+    }
+
     // concat->word | {expr}
-    fn concat(&self) -> Result<ParseTreeNode, String> {}
+    fn concat(&self, lexer: &mut Lexer) -> Result<ParseTreeNode, String> {
+        Err(String::new())
+    }
 }
 
 fn main() {
